2e0a922d56166919506b85f5bc76909e0964385a
siyfa <siyanbolafaruk@gmail.com>
2023-08-14 12:16:12 +0100

    testing

--- src/page/chat1.js
+++ src/page/chat1.js
@@ -5,55 +5,75 @@ import { useEffect, useState, useRef } from "react";
 function Chat1() {
   const [input, setInput] = useState("");
   const [history, setHistory] = useState([]);
-  const [chat, setChat] = useState([]);
+  const [chat, setChat] = useState({});
   const [messages, setMessages] = useState([]);
   const [arrivalMsg, setArrivalMsg] = useState([])
 
-  
-  const socket = useRef(io.connect("https://localhost:8000"));
-  const userId = '61b9d556623081424535539c'
+  const socket = useRef(io.connect("http://localhost:7500"));
+  const userId = 'customer1059152b4365c8695b217'
 
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWI5ZDU1NjYyMzA4MTQyNDUzNTUzOWMiLCJpYXQiOjE2NDEyODg1Mzh9.iXriBIiGghK3lyIG2nu1Y1x47ZHF4nNUph72Kg_MT8w";
-  
-  useEffect(()=>{
-    socket.current = io.connect('https://localhost:8000');
-    socket.current.on('getMessage', (data)=>{
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXVpZCI6ImN1c3RvbWVyMTA1OTE1MmI0MzY1Yzg2OTViMjE3IiwiZW1haWwiOiJjdXN0b21lcnVzZXIxQGdtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciIsInVzZXJUeXBlIjoidGVuYW50IiwiaWF0IjoxNjkyMDA1Mjg0LCJleHAiOjE3MDQxMDEyODR9.QqnfTlUu0sh4bv7oXK3zZ4CgHKCqQC9NzuQo7GUNjCw";
+
+  useEffect(() => {
+    socket.current = io.connect('http://localhost:7500');
+    socket.current.on('getMessage', (data) => {
       console.log("data", data)
       setArrivalMsg({
-        sender: data.senderId,
+        sender: data.sender,
         text: data.text,
         createdAt: Date.now()
       })
     })
-  },[])
-
-  useEffect(()=>{
-    arrivalMsg && chat.members?.includes(arrivalMsg.sender) &&
-    setMessages((prev) => [...prev, arrivalMsg])
-  },[arrivalMsg, chat?.members])
- 
-  useEffect(()=>{
-    socket.current.emit("addUser", userId)
-    socket.current.on("getUsers", (users)=>{
-      console.log("online users", users)
-    })
-  },[userId])
+  }, [])
 
   useEffect(() => {
-    const chatHistory = async () => {
-      const res = await axios.get(
-        `https://localhost:8000/api/chats/${userId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      setHistory(res.data);
+    const getMessages = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:7500/api/v1/messaging/chat/rider142851b27e50bd3d1379d`,
+          {
+            headers: {
+              'x-auth-token': token,
+            },
+          }
+        );
+        const resp = res.data.data.chatFound;
+        setMessages(JSON.parse(resp.chat.messages))
+        setChat(resp.chat)
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
     };
-    chatHistory();
+    getMessages();
   }, []);
+
+  useEffect(() => {
+    arrivalMsg && chat.members?.includes(arrivalMsg.sender) &&
+      setMessages((prev) => [...prev, arrivalMsg])
+  }, [arrivalMsg])
+
+  useEffect(() => {
+    socket.current.emit("addUser", userId)
+    socket.current.on("getUsers", (users) => {
+      console.log("online users", users)
+    })
+  }, [userId])
+
+  // useEffect(() => {
+  //   const chatHistory = async () => {
+  //     const res = await axios.get(
+  //       `http://localhost:7500/api/v1/messagings/${userId}`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+  //     setHistory(res.data);
+  //   };
+  //   chatHistory();
+  // }, []);
 
   const formHandler = async (e) => {
     e.preventDefault();
@@ -62,30 +82,30 @@ function Chat1() {
       text: input,
     };
 
-    const receiverId = chat?.members.find((member) => member !== userId)
+    const receiver = chat?.members.find((member) => member !== userId)
 
     socket.current.emit('sendMessage', {
-      senderId: userId,
-      receiverId,
+      sender: userId,
+      receiver,
       text: input
     })
 
     const res = await axios.post(
-      `https://localhost:8000/api/chat/new/message/61d30d5c82ea500340a87b3e/${userId}`,
+      `http://localhost:7500/api/v1/messaging/send/1`,
       newMsg,
       {
         headers: {
-          Authorization: `Bearer ${token}`,
+          'x-auth-token': token,
         },
       }
     );
-    setMessages([...messages, res.data]);
+    setMessages([...messages, res.data.data.newMessage]);
     setInput("");
   };
 
   const getChat = async (id) => {
     const res = await axios.get(
-      `https://localhost:8000/api/chat/messages/${id}/${userId}`,
+      `http://localhost:7500/api/v1/messaging/chat/${id}/${userId}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -101,16 +121,15 @@ function Chat1() {
       <h1> User one </h1>{" "}
       <div className="history">
         <h3> Chat History </h3>{" "}
-        <div>
-          {" "}
+        {/* <div>
           {history.map((h, i) => (
             <div className="msg" key={i}>
               <p> {h.firstName + " " + h.lastName} </p>{" "}
               <span> {h.lastMsg} </span>{" "}
               <button onClick={(e) => getChat(h.id)}> chat </button>{" "}
             </div>
-          ))}{" "}
-        </div>{" "}
+          ))}
+        </div> */}
       </div>
       <div>
         {" "}
@@ -119,7 +138,7 @@ function Chat1() {
         ))}{" "}
       </div>
       <div>
-        <ul id="messages"> </ul>{" "}
+        <ul id="messages"> </ul>
         <form id="form" action="" onSubmit={formHandler}>
           <input
             id="input"
--- src/page/chat2.js
+++ src/page/chat2.js
@@ -5,54 +5,74 @@ import { useEffect, useState, useRef } from "react";
 function Chat2() {
   const [input, setInput] = useState("");
   const [history, setHistory] = useState([]);
-  const [chat, setChat] = useState([]);
+  const [chat, setChat] = useState({});
   const [arrivalMsg, setArrivalMsg] = useState([])
   const [messages, setMessages] = useState([])
 
-  const socket = useRef(io.connect("https://localhost:8000"));
-  const userId = '61b21986fb24d4630cde57f3'
+  const socket = useRef(io.connect("http://localhost:7500"));
+  const userId = 'rider142851b27e50bd3d1379d'
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWI5ZDU1NjYyMzA4MTQyNDUzNTUzOWMiLCJpYXQiOjE2NDEyODg1Mzh9.iXriBIiGghK3lyIG2nu1Y1x47ZHF4nNUph72Kg_MT8w";
-  
-    useEffect(()=>{
-      socket.current = io.connect('https://localhost:8000');
-      socket.current.on('getMessage', (data)=>{
-        console.log("data", data)
-        setArrivalMsg({
-          sender: data.senderId,
-          text: data.text,
-          createdAt: Date.now()
-        })
-      })
-    },[])
-  
-    useEffect(()=>{
-      arrivalMsg && chat.members?.includes(arrivalMsg.sender) &&
-      setMessages((prev) => [...prev, arrivalMsg])
-    },[arrivalMsg, chat?.members])
-   
-    useEffect(()=>{
-      socket.current.emit("addUser", userId)
-      socket.current.on("getUsers", (users)=>{
-        console.log("users", users)
-      })
-    },[userId])
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXVpZCI6InJpZGVyMTQyODUxYjI3ZTUwYmQzZDEzNzlkIiwiZW1haWwiOiJyaWRlcnVzZXIxQGdtYWlsLmNvbSIsInJvbGUiOiJyaWRlciIsInVzZXJUeXBlIjoidGVuYW50IiwiaWF0IjoxNjkyMDA1NDg3LCJleHAiOjE3MDQxMDE0ODd9.zvS92TWgzXBCcNXyVG10S9OuSMJZGr_TClNRi6RKgnA"
 
   useEffect(() => {
-    const chatHistory = async () => {
-      const res = await axios.get(
-        `https://localhost:8000/api/chats/${userId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      setHistory(res.data);
+    socket.current = io.connect('http://localhost:7500');
+    socket.current.on('getMessage', (data) => {
+      console.log("data", data)
+      setArrivalMsg({
+        sender: data.sender,
+        text: data.text,
+        createdAt: new Date
+      })
+    })
+  }, [])
+
+  useEffect(() => {
+    arrivalMsg && chat.members?.includes(arrivalMsg.sender) &&
+      setMessages((prev) => [...prev, arrivalMsg])
+  }, [arrivalMsg])
+
+  useEffect(() => {
+    socket.current.emit("addUser", userId)
+    socket.current.on("getUsers", (users) => {
+      console.log("users", users)
+    })
+  }, [userId])
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:7500/api/v1/messaging/chat/customer1059152b4365c8695b217`,
+          {
+            headers: {
+              'x-auth-token': token,
+            },
+          }
+        );
+        const resp = res.data.data.chatFound;
+        setMessages(JSON.parse(resp.chat.messages))
+        setChat(resp.chat)
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
     };
-    chatHistory();
+    getMessages();
   }, []);
+
+  // useEffect(() => {
+  //   const chatHistory = async () => {
+  //     const res = await axios.get(
+  //       `http://localhost:7500/api/chats/${userId}`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+  //     setHistory(res.data);
+  //   };
+  //   chatHistory();
+  // }, []);
 
   const formHandler = async (e) => {
     e.preventDefault();
@@ -61,30 +81,30 @@ function Chat2() {
       text: input,
     };
 
-    const receiverId = chat?.members.find((member) => member !== userId)
+    const receiver= chat?.members.find((member) => member !== userId)
 
     socket.current.emit('sendMessage', {
-      senderId: userId,
-      receiverId,
+      sender: userId,
+      receiver,
       text: input
     })
 
     const res = await axios.post(
-      `https://localhost:8000/api/chat/new/message/61d30d5c82ea500340a87b3e/${userId}`,
+      `http://localhost:7500/api/v1/messaging/send/1`,
       newMsg,
       {
         headers: {
-          Authorization: `Bearer ${token}`,
+          'x-auth-token': token,
         },
       }
     );
-    setMessages([...messages, res.data]);
+    setMessages([...messages, res.data.data.newMessage]);
     setInput("");
   };
 
   const getChat = async (id) => {
     const res = await axios.get(
-      `https://localhost:8000/api/chat/messages/${id}/${userId}`,
+      `http://localhost:7500/api/chat/messages/${id}/${userId}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
